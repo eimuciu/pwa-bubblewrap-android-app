f8c3836660feba65f35951c8b28b068270e2ae21
eimuciu <allgoodata@outlook.com>
2024-09-15 09:10:15 +0300

    init commit

---
+++ app/src/main/java/app/vercel/myetracker/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.myetracker.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
